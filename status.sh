86f869108eb0161ea27ff474e446c7a34107685d
ION606 <ion606@cog-218.dynamic2.rpi.edu>
2024-06-26 12:34:01 -0400

    added status command

--- status.sh
+++ status.sh
@@ -14,16 +14,7 @@ fi
 source "$HOME/ionsrc/creds.txt"
 source "$PWD/.ionvcs/src.config"
 
-# Set the remote path for rsync
 REMOTE_PATH="$csrc"
-
-while getopts ":of" opt; do
-    case $opt in
-        f)
-            force_flag=true
-            ;;
-    esac
-done
 
 # Set the destination folder
 if [ -n "$2" ]; then
@@ -32,18 +23,13 @@ else
     DEST_FOLDER="$PWD"
 fi
 
-
 # Use rsync with sshpass to check for differences without copying
 DIFF_OUTPUT=$(/usr/bin/sshpass -p "$password" rsync -avcn --delete -e ssh "$REMOTE_PATH" "$DEST_FOLDER")
 
 # Check if there are differences
-if [ ! -z "$DIFF_OUTPUT" && ! $force_flag ]; then
-    echo "conflicts found between the local and remote directories (make sure they're correct and re-run using the -f flag):"
+if [ ! -z "$DIFF_OUTPUT" ]; then
     echo "$DIFF_OUTPUT"
     exit 1;
+else
+    echo "no changes found!"
 fi
-
-# Use rsync with sshpass to copy files with a progress bar
-echo -n "cloning..."
-/usr/bin/sshpass -p "$password" rsync -avcn --exclude-from="$PWD/.ionign" --info=progress2 --no-i-r -h -e ssh "$DEST_FOLDER" "$REMOTE_PATH" || { echo "failed to clone!"; exit 1; }
-echo "done!"
